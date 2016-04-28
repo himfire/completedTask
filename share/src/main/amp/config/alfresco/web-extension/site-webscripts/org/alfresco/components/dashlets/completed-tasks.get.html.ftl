dd490de2967d2c49a101cd55d603b6e7f508db28
Hisham <hishamfire@gmail.com>
2016-04-28 12:34:08 +0300

    Initial Checkin

--- share/src/main/amp/config/alfresco/web-extension/site-webscripts/org/alfresco/components/dashlets/completed-tasks.get.html.ftl
+++ share/src/main/amp/config/alfresco/web-extension/site-webscripts/org/alfresco/components/dashlets/completed-tasks.get.html.ftl
@@ -1,57 +1,34 @@
-<@markup id="css" >
-   <#-- CSS Dependencies -->
-   <@link rel="stylesheet" type="text/css" href="${url.context}/res/components/dashlets/my-tasks.css" group="dashlets"  />
-</@>
-
-<@markup id="js">
-   <#-- JavaScript Dependencies -->
-   <@script type="text/javascript" src="${url.context}/res/components/workflow/workflow-actions.js" group="dashlets"/>
-   <@script type="text/javascript" src="${url.context}/res/components/dashlets/my-tasks.js" group="dashlets"/>
-</@>
-
-<@markup id="widgets">
-   <@createWidgets group="dashlets"/>
-</@>
-
-<@markup id="html">
-   <@uniqueIdDiv>
-      <#assign id = args.htmlid?html>
-        <div style="width: 30%;float: left">
-            <div>
-                <form action="">
-                    <ul>
-                        <li>Due
-                            <br />
-                            <select>
-                                <option value="today">Today</option>
-                                <option value="tomorrow">Tomorrow</option>
-                                <option value="next_7_days">Next 7 days</option>
-                                <option value="overdue">Overdue</option>
-                                <option value="no_date">No date</option>
-                            </select>
-                        </li>
-                        <li>Periority
-                            <br />
-                            <select>
-                                <option value="high">High</option>
-                                <option value="medium">Medium</option>
-                                <option value="low">Low</option>
-                            </select>
-                        </li>
-                    </ul>
-                    <input type="button" value="submit" />
-                </form>
-            </div>
-        </div>
-        <table style="width: 70%">
-            <tr style="border:2px solid black">        
-                <td>1</td>
-                <td>hello</td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>hello</td>
-            </tr>
-        </table>
-   </@>
-</@>
+<div class="dashlet">  
+	<div class="title">${msg("header")}</div>  
+	<div class="body scrollableList">  
+		<div class="detail-list-item first-item last-item">  
+			<div class="toolbar flat-button">
+				<span class="first-child">
+					<span>Due date</span>
+                     <span>
+                     	<button tabindex="0" type="button" aria-haspopup="true">==Select== ▾</button>
+                     </span>
+             	</span>
+				<span class="first-child">
+                     <span>Periority</span>
+                     <span>
+                     	<button tabindex="0" type="button" aria-haspopup="true">==Select== ▾</button>
+                     </span>
+              	</span>
+				<span class="first-child">
+                     <span>Due date</span>
+                     <span>
+                     	<button tabindex="0" type="button" aria-haspopup="true">==Select== ▾</button>
+                     </span>
+             	</span>                                    
+				<div class="clear"></div>
+			</div>
+			<div class="completed-task-body">
+				${msg("header")}<br />
+				<#list tasks as task>
+					${task.id} ${task.url}
+				</#list>		
+			</div>
+		</div>  
+	</div>  
+ </div> 
--- share/src/main/java/com/hopenshare/controller/CompletedTask.java
+++ share/src/main/java/com/hopenshare/controller/CompletedTask.java
@@ -1,11 +1,14 @@
 package com.hopenshare.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.extensions.surf.FrameworkUtil;
 import org.springframework.extensions.surf.RequestContext;
-import org.springframework.extensions.surf.exception.ConnectorServiceException;
-import org.springframework.extensions.surf.exception.UserFactoryException;
 import org.springframework.extensions.surf.support.ThreadLocalRequestContext;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.DeclarativeWebScript;
@@ -16,52 +19,53 @@ import org.springframework.extensions.webscripts.connector.ConnectorContext;
 import org.springframework.extensions.webscripts.connector.HttpMethod;
 import org.springframework.extensions.webscripts.connector.Response;
 
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
-import org.codehaus.jackson.map.ObjectMapper;
-
 public class CompletedTask extends DeclarativeWebScript {
+	String title;
+	String dueDate;
+	String message;
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
 		RequestContext context = (RequestContext) ThreadLocalRequestContext.getRequestContext();
 		Connector conn;
+		List<Map<String, Object>> tasksList = new ArrayList<Map<String, Object>>();
 		try {
-			//initialize connection
+			// initialize connection
 			conn = FrameworkUtil.getConnector(context, "alfresco");
 			ConnectorContext c = new ConnectorContext(HttpMethod.GET);
 			c.setContentType("application/json");
-			//it's for connecting to the url http://localhost:8080/alfresco/service/api/task-instances
-			Response res = conn.call("/s/api/task-instances", c, null);
+			// it's for connecting to the url
+			// http://localhost:8080/alfresco/service/api/task-instances
+			Response res = conn.call("/api/task-instances", c, null);
+			System.out.println(res.getStatus().getCode());
 			if (Status.STATUS_OK == res.getStatus().getCode()) {
 				System.out.println(res.getResponse());
-				//here I'm using jackson to read from url, I tried my code with some file I took from the url above
-				//and paste it in file called file1.json
-				//and I add the dependency for json response
-				try {
-					ObjectMapper mapper = new ObjectMapper();
-					//String jsonString = mapper.readTree(new File("K:\\alfresco code\\json-reader\\file1.json"))
-					JsonNode jsonString = mapper.readTree(res.getResponse());
-					JsonNode root = jsonString.path("data");
-					if (root.isMissingNode()) {
-			 			System.out.println("it's wrong");
-			 		}else
-			 		{
-			 			int i=0;
-			 			for (JsonNode node : root) {
-			 				String title = node.path("title").asText();
-			 				String dueDate = node.path("dueDate").asText();
-			 				String message = node.path("message").asText();
-			 				JsonNode Initiator = root.path("initiator");
-			 				System.out.println(i+" title : " + title+",message : " + message+",dueDate : "+dueDate);
-			 			}
-			 		}
-			 	} catch (JsonParseException e) { e.printStackTrace(); }
-			      catch (JsonMappingException e) { e.printStackTrace(); }
+				JSONObject globalResponse = new JSONObject(res.getResponse());
+				JSONArray tasksArray = globalResponse.getJSONArray("data");
+				
+				for(int i=0; i< tasksArray.length(); i++){
+					Map<String, Object> taskModel = new HashMap<String, Object>();
+					JSONObject taskObject = tasksArray.getJSONObject(i);
+					//TODO charge the task model with the desired properties
+					taskModel.put("id", taskObject.getString("id"));
+					taskModel.put("url", taskObject.getString("url"));
+					taskModel.put("name", taskObject.getString("name"));
+					taskModel.put("title", taskObject.getString("title"));
+					taskModel.put("state", taskObject.getString("state"));					
+					tasksList.add(taskModel);
+				}
+
+			} else {
+				System.out.println("Not fetched " + res.getStatus().getCode());
 			}
-		} catch(Exception e){
-			
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		return null;
+		Map<String, Object> fmModel = new HashMap<String, Object>();
+		fmModel.put("tasks", tasksList);
+		//fmModel.put("fromJava", "HelloFromJava");
+		return fmModel;
+
 	}
 }
